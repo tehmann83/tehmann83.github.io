263bec5518da6405dca546cb27de872f70b89611
tehmann83 <ehmannth@gmail.com>
2017-05-28 11:12:49 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var projects = {
   "projects" : [{
       "title" : "Feedreader Testing",
       "url" : "p06-feed-reader-testing/index.html",
+      "github": "https://github.com/tehmann83/tehmann83.github.io/tree/master/p06-feed-reader-testing",
       "pic" : "img/p06.PNG",
       "description" : "descr",
       "usedTechnology" : "used tech"
@@ -9,6 +10,7 @@ var projects = {
     {
       "title" : "Neighborhood Map",
       "url" : "p05-neighborhood-map/index.html",
+      "github": "https://github.com/tehmann83/tehmann83.github.io/tree/master/p05-neighborhood-map",
       "pic" : "img/p05.PNG",
       "description" : "",
       "usedTechnology" : ""
@@ -16,6 +18,7 @@ var projects = {
     {
       "title" : "Website Optimization",
       "url" : "p04-website-optimization/index.html",
+      "github": "https://github.com/tehmann83/tehmann83.github.io/tree/master/p04-website-optimization",
       "pic" : "img/p04.PNG",
       "description" : "",
       "usedTechnology" : ""
@@ -23,6 +26,7 @@ var projects = {
     {
       "title" : "Classic Arcade Game",
       "url" : "p03-classic-arade-game/index.html",
+      "github": "https://github.com/tehmann83/tehmann83.github.io/tree/master/p03-classic-arcade-game",
       "pic" : "img/p03.PNG",
       "description" : "",
       "usedTechnology" : ""
@@ -30,6 +34,7 @@ var projects = {
     {
       "title" : "Online Resume",
       "url" : "p02-online-resume/index.html",
+      "github": "https://github.com/tehmann83/tehmann83.github.io/tree/master/p02-online-resume",
       "pic" : "img/p02.PNG",
       "description" : "",
       "usedTechnology" : ""
@@ -37,6 +42,7 @@ var projects = {
     {
       "title" : "Portfolio Site",
       "url" : "p01-portfolio-site/index.html",
+      "github": "https://github.com/tehmann83/tehmann83.github.io",
       "pic" : "img/p01.PNG",
       "description" : "",
       "usedTechnology" : ""
@@ -49,45 +55,48 @@ var projects = {
 var HTMLprojectFeatured = '<div class="row featured-project-entry"</div>';
 var HTMLprojectStart = '<div class="row projects-entry"></div>';
 
-var HTMLprojectPic = '<img src="%data%" class="col-md-6 project-pic">';
+var HTMLprojectImage = '<img src="%data%" class="col-md-6 project-pic">';
+var HTMLprojectImageLink = '<a href="projects.html"><img src="%data%" class="col-md-6 project-pic"></a>';
 
-
-
-/* TODO: Connect title with url.
- * TODO: In a row two columns with col-md-6. Left: Clickable picture,
- *       Right: Title/URL, Description, Links to website and github EXTRA
- *                                                      via button!
- *
-*/
 var HTMLprojectTitle = '<h3>%data%</h3>';
+var HTMLprojectTitleLink = '<a href="projects.html"><h3>%data%</h3></a>';
+
 var HTMLprojectUrl = '<a href="#">%data%</a>';
 var HTMLprojectDescription = '<h4>Description:</h4>'+'<p> ' + '%data%' + '</p>';
 var HTMLprojectUsedTechnology = '<h4>Used Technology:</h4>'+'<p> ' + '%data%' + '</p>';
 
+/* TODO: Add buttons to website and github
+*/
+var HTMLprojectButton= '<a class="btn btn-default" href="%data%" role="button">Website</a>';
+var HTMLprojectGithub = '<a class="btn btn-default" href="%data%" role ="button">Github</a>';
+
 /* Display projects function */
 projects.display = function () {
   for (var i = 0; i < 1; i++){
+    // Add to index.html with links to projects.html
     $("#featured-project").append(HTMLprojectFeatured);
 
-    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
+    var formattedTitle = HTMLprojectTitleLink.replace("%data%", projects.projects[i].title);
     var formattedUrl = HTMLprojectUrl.replace("%data%", projects.projects[i].url);
-    var formattedPic = HTMLprojectPic.replace("%data%", projects.projects[i].pic);
+    var formattedPic = HTMLprojectImageLink.replace("%data%", projects.projects[i].pic);
     var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
     var formattedUsedTechnology = HTMLprojectUsedTechnology.replace("%data%", projects.projects[i].usedTechnology);
 
-    $(".featured-project-entry").append(formattedPic, formattedTitle, formattedUrl, formattedDescription, formattedUsedTechnology);
+    $(".featured-project-entry").append(formattedPic, formattedTitle, formattedDescription, formattedUsedTechnology);
   }
 
+  // Add to projects.html
   for(var i = 0; i < projects.projects.length; i++){
     $("#projects").append(HTMLprojectStart);
 
     var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
     var formattedUrl = HTMLprojectUrl.replace("%data%", projects.projects[i].url);
-    var formattedPic = HTMLprojectPic.replace("%data%", projects.projects[i].pic);
+    var formattedPic = HTMLprojectImage.replace("%data%", projects.projects[i].pic);
     var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
     var formattedUsedTechnology = HTMLprojectUsedTechnology.replace("%data%", projects.projects[i].usedTechnology);
-
-    $(".projects-entry:last").append(formattedPic, formattedTitle, formattedUrl, formattedDescription, formattedUsedTechnology);
+    var formattedButton = HTMLprojectButton.replace("%data%", projects.projects[i].url);
+    var formattedGithub = HTMLprojectGithub.replace("%data%", projects.projects[i].github);
+    $(".projects-entry:last").append(formattedPic, formattedTitle, formattedDescription, formattedUsedTechnology, formattedButton, formattedGithub);
   }
 };
 
